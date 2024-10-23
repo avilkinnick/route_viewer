af91293c5ed37d72e2a799152b46fd4a38b44c48
avilkinnick <avilkin.nick@gmail.com>
2024-10-23 09:44:42 +0300

    Get instance extensions

--- include/VulkanInstance.h
+++ include/VulkanInstance.h
@@ -3,12 +3,37 @@
 
 #include <cstdint>
 
+#include <map>
+#include <optional>
+#include <set>
+#include <string>
+#include <string_view>
+
 #include <vulkan/vulkan_core.h>
 
 class VulkanInstance
 {
+private:
+    using CLayerName = const char*;
+    using LayerName = std::string;
+    using LayerNameView = std::string_view;
+    using LayerNameSet = std::set<LayerName>;
+
+    using CExtensionName = const char*;
+    using ExtensionName = std::string;
+    using ExtensionMap = std::map<ExtensionName, std::optional<LayerNameView>>;
+
 public:
     VulkanInstance(const char* application_name, std::uint32_t application_version);
+
+private:
+    LayerNameSet get_available_layer_names() const;
+    ExtensionMap get_available_extensions(const LayerNameSet& available_layer_names) const;
+
+    void get_available_extensions_from_layer(
+        ExtensionMap& available_extensions,
+        std::optional<LayerNameView> layer_name = std::nullopt
+    ) const;
 
 private:
     VkInstance instance;
--- src/VulkanInstance.cpp
+++ src/VulkanInstance.cpp
@@ -2,6 +2,13 @@
 
 #include <cstdint>
 
+#include <map>
+#include <optional>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include <vulkan/vulkan_core.h>
 
 VulkanInstance::VulkanInstance(const char* application_name, std::uint32_t application_version)
@@ -18,6 +25,9 @@ VulkanInstance::VulkanInstance(const char* application_name, std::uint32_t appli
     application_info.engineVersion = 0;
     application_info.apiVersion = api_version;
 
+    const auto available_layer_names = get_available_layer_names();
+    const auto available_extensions = get_available_extensions(available_layer_names);
+
     VkInstanceCreateInfo create_info;
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     // create_info.pNext;
@@ -27,4 +37,59 @@ VulkanInstance::VulkanInstance(const char* application_name, std::uint32_t appli
     // create_info.ppEnabledLayerNames;
     // create_info.enabledExtensionCount;
     // create_info.ppEnabledExtensionNames;
+}
+
+VulkanInstance::LayerNameSet VulkanInstance::get_available_layer_names() const
+{
+    std::uint32_t available_layer_count;
+    vkEnumerateInstanceLayerProperties(&available_layer_count, nullptr);
+
+    std::vector<VkLayerProperties> available_layers_properties(available_layer_count);
+    vkEnumerateInstanceLayerProperties(&available_layer_count, available_layers_properties.data());
+
+    LayerNameSet available_layer_names;
+    for (const auto& available_layer_properties : available_layers_properties)
+    {
+        available_layer_names.emplace(available_layer_properties.layerName);
+    }
+
+    return available_layer_names;
+}
+
+VulkanInstance::ExtensionMap VulkanInstance::get_available_extensions(const LayerNameSet& available_layer_names) const
+{
+    ExtensionMap available_extensions;
+    get_available_extensions_from_layer(available_extensions);
+
+    for (std::string_view available_layer_name : available_layer_names)
+    {
+        get_available_extensions_from_layer(available_extensions, available_layer_name);
+    }
+
+    return available_extensions;
+}
+
+void VulkanInstance::get_available_extensions_from_layer(
+    ExtensionMap& available_extensions,
+    std::optional<LayerNameView> layer_name
+) const
+{
+    std::uint32_t available_extension_count;
+    vkEnumerateInstanceExtensionProperties(
+        layer_name ? layer_name->data() : nullptr,
+        &available_extension_count,
+        nullptr
+    );
+
+    std::vector<VkExtensionProperties> available_extensions_properties(available_extension_count);
+    vkEnumerateInstanceExtensionProperties(
+        layer_name ? layer_name->data() : nullptr,
+        &available_extension_count,
+        available_extensions_properties.data()
+    );
+
+    for (const auto& available_extension_properties : available_extensions_properties)
+    {
+        available_extensions.emplace(available_extension_properties.extensionName, layer_name);
+    }
 }
